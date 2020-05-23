951eb7b5a801564acb5f3922d4043643880602b0
namh <namhun.song@cocktailfd.com>
2020-05-23 23:25:06 +0900

    Add test files

---
+++ test/task/test_tasks.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(
+    os.path.abspath(__file__)),  '..', '..'))
+
+import unittest
+from unittest.mock import MagicMock
+from unittest.mock import ANY
+
+from gigas.celconfig import DEBUG
+from gigas.celery import app
+from gigas.task.tasks import xsum
+
+class TestAddTask(unittest.TestCase):
+    def setUp(self):
+        self.result = xsum([3, 5])
+        # self.results = self.task.get()  
+
+    def test_addition(self):
+        res = xsum([3, 5])
+        self.assertEqual(res, 8)
+        self.assertEqual(self.result, 8)
+        
